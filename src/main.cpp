4af4398f08a464dceb4edbeb1c8bc42efa24aeca
Michal <michaloracki@wp.pl>
2020-06-03 14:17:25 +0200

    Fix one line in point initialization

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ int main()
 	RUNALLTESTS();
 
 	// tests for instantination
-	Point<int>(1, 2);	// OK
+	Point<int> p(1, 2);	// OK
+	std::cout << p.x;
 	//Point<std::string>("dsa", "DSA");	// ERROR - must be numeric
 	//Figure<int, 5> figure;	// ERROR - cannot instantinate abstract class
 	Figure<int, 4>* f_sq = new Square<Point<int>>(Point<int>(1, 2), Point<int>(3, 5));	//OK
